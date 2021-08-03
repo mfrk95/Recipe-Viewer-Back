1fb5b5cc7ecdd6ab5f9452a2063b9cb8b12997f9
mfrk95 <mfrk95@gmail.com>
2021-08-03 20:33:34 -0300

    Cambio en servicio de recipesAll

--- src/main/java/com/example/recipeviewerback/rest/RecipeController.java
+++ src/main/java/com/example/recipeviewerback/rest/RecipeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
@@ -16,18 +17,23 @@ public class RecipeController {
     private RecipeRepository recipeRepository;
 
     @Autowired
-    public RecipeController(RecipeRepository recipeRepository){
+    public RecipeController(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
 
     @GetMapping("/recipesAll")
-    public List<Recipe> findAll(){
-        List<Recipe> recipeList = recipeRepository.findAll();
-        return recipeList;
+    public List<Recipe> findAll(@RequestParam(required = false) String category) {
+        List<Recipe> allList = recipeRepository.findAll();
+        List<Recipe> categoryList = allList.stream().filter(recipe -> recipe.getCategory().equals(category)
+        ).collect(Collectors.toList());
+        if (category != null)
+            return categoryList;
+        else
+            return allList;
     }
 
     @PostMapping("/addRecipe")
-    public String addRecipe( MultipartFile file, @RequestParam String title, @RequestParam String description, @RequestParam String category) {
+    public String addRecipe(MultipartFile file, @RequestParam String title, @RequestParam String description, @RequestParam String category) {
 
         String path = "uploads/";
         String fileName = file.getOriginalFilename();
@@ -37,13 +43,14 @@ public class RecipeController {
         }
         try {
             file.transferTo(dest); // save document
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
-        Recipe rcp = new Recipe(title, description,category,dest.getAbsolutePath());
+        Recipe rcp = new Recipe(title, description, category, dest.getAbsolutePath());
         recipeRepository.save(rcp);
         return "Uploaded successfully!";
     }
 
 }
+
 
